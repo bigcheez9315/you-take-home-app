5b0c5f32eb224504a82404ff3bfecd9d7a9526d0
Avi Steinberg <asteinberg@gala.games>
2024-02-01 18:40:03 -0500

    changing port in dockerfile and express app to try to get it to generate a new image

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const express = require('express');
 // dotenv.config();
 
 const app = express();
-const port = 8080;
+const port = 5000;
 
 app.get('/', (req, res) => {
   res.send('Express + TypeScript Server');
