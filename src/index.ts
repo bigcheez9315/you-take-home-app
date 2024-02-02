3eea2af749a941b30e407f1950421d27cab225ee
Avi Steinberg <asteinberg@gala.games>
2024-02-01 19:05:53 -0500

    going back to 8080 port

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const express = require('express');
 // dotenv.config();
 
 const app = express();
-const port = 5000;
+const port = 8080;
 
 app.get('/', (req, res) => {
   res.send('You take-home app welcome page!');
